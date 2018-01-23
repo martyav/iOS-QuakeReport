cbc1bff16c8bc030a3bd3e6ffa87fae54af95f73
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-01-23 00:01:22 -0500

    put json in own file, completed the parser, need to write test

--- iOS-QuakeReport/iOS-QuakeReportTests/iOS_QuakeReportTests.swift
+++ iOS-QuakeReport/iOS-QuakeReportTests/iOS_QuakeReportTests.swift
@@ -10,10 +10,12 @@ import XCTest
 @testable import iOS_QuakeReport
 
 class iOS_QuakeReportTests: XCTestCase {
+    var exampleParser: JSONParser!
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        self.exampleParser = JSONParser.instance
     }
     
     override func tearDown() {
@@ -21,16 +23,8 @@ class iOS_QuakeReportTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testParser() {
+        // take in the geojsonfile, return an array of earthquakes
     }
     
 }
